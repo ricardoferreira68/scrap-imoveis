915a11318b117f6ae190e5b7b9dca1962f511463
Ricardo Ferreira <ricardo@debian.hildinair.com.br>
2024-06-01 17:06:21 -0300

    Refact: app.py

--- scrap_imoveis/app.py
+++ scrap_imoveis/app.py
@@ -1,42 +1,64 @@
 import time
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.common.exceptions import NoSuchElementException
-link_list = ["https://www.zapimoveis.com.br/venda/apartamentos/pa+belem/?transacao=venda&onde=,Par%C3%A1,Bel%C3%A9m,,,,,city,BR%3EPara%3ENULL%3EBelem,-1.456343,-48.501299,&tipos=apartamento_residencial&itl_id=1000072&itl_name=zap_-_botao-cta_buscar_to_zap_resultado-pesquisa",]
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
-def fetch_links(link):
-    service = Service()
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.maximize_window()
-    driver.get(link)
-    actions = ActionChains(driver)
+
+def fetch(driver):
+    listing_wrapper__content = driver.find_element(By.CLASS_NAME, "listing-wrapper__content")
+    delta_y = listing_wrapper__content.rect['y']
     for _ in range(100):
-        actions.send_keys(Keys.SPACE).perform()
-        time.sleep(0.5)
-
-    driver.implicitly_wait(60)
-
-    bottomFooter = driver.find_element(By.CLASS_NAME, 'footer')
-    driver.execute_script('arguments[0].scrollIntoView();', bottomFooter)
-
-    apartment = driver.page_source.count("<div data-position=")
-    print(apartment)
-
-    driver.implicitly_wait(60)
-    
-    for i in range(1, apartment+1):
+        ActionChains(driver).scroll_by_amount(0, delta_y).perform()
+        time.sleep(1)
+    driver.implicitly_wait(5)
+    number_of_apartments = driver.page_source.count("<div data-position=")
+    print(number_of_apartments)
+    driver.implicitly_wait(5)
+    for i in range(1, number_of_apartments+1):
         try:
             item = driver.find_element(By.XPATH, '//*[@id="__next"]/main/section/div/form/div[2]/div[4]/div[1]/div/div['+str(i)+']/div/a').get_attribute('href')
-            print(item)
-            time.sleep(1)
+            # print(item)
+            with open('apartment_links.txt', 'a') as file_apartment_links:
+                file_apartment_links.write(item+'\n')
+            time.sleep(0.1)
         except NoSuchElementException:
             continue
-    # driver.implicitly_wait(100)
 
-    '//*[@id="__next"]/main/section/div/form/div[2]/div[4]/div[1]/div/section/nav/button[2]'
-
-fetch_links(link_list[0])
+link = 'https://www.zapimoveis.com.br/venda/apartamentos/pa+belem/?__ab=exp-aa-test:control,novopos:new,super-high:new,olx:control,score-rkg:control&transacao=venda&onde=,Par%C3%A1,Bel%C3%A9m,,,,,city,BR%3EPara%3ENULL%3EBelem,-1.456343,-48.501299,&tipos=apartamento_residencial&pagina=55'
+service = Service()
+options = webdriver.ChromeOptions()
+options.add_argument('--disable-notifications')
+options.add_argument('--disable-geolocation')
+driver = webdriver.Chrome(service=service, options=options)
+driver.maximize_window()
+driver.get(link)
+time.sleep(10)
+try:
+    cookei_alert = driver.find_element(By.XPATH, '//*[@id="cookie-notifier-cta"]')  
+    cookei_alert.click()
+except Exception as e:
+    print("no cookie popup", e)
+page = 0
+while True:
+    page += 1
+    print(f'Page: {page}')
+    fetch(driver)
+    pagination_button_xpath = '//*[@id="__next"]/main/section/div/form/div[2]/div[4]/div[1]/div/section/nav/button[2]'
+    try:
+        driver.find_element(By.XPATH, pagination_button_xpath)
+    except Exception as e:
+        print(repr(e))
+        break
+    try:
+        pagination_button = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, pagination_button_xpath)))
+        # print(pagination_button)
+        pagination_button.click()
+    except Exception as e:
+        print(e)
+        break
+    time.sleep(5)
+driver.quit()
